5b7d3b5b39a73c5059e4e2b9d969181bda6dabad
Nathan Jones <Purkinje90@gmail.com>
2015-09-16 16:52:08 -0600

    Formatting

---
+++ src/scripts/show-student-number/js/main.js
@@ -0,0 +1,15 @@
+export function main() {
+  var scriptHasRun = false;
+  if (!scriptHasRun) {
+    scriptHasRun = true;
+    var contentHeader = $j('#content').find('h1');
+    if (contentHeader.length > 0) {
+      // If there is more than one header, just modify the first one.
+      contentHeader = contentHeader.eq(0);
+      var currentText = contentHeader.text();
+      var addedText = ' (Student Number: ' + psData.studentNumber + ')';
+      var newText = currentText + addedText;
+      contentHeader.text(newText);
+    }
+  }
+}
