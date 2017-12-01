39fb238f890b75cdec5e10ffde7419127f61d33a
Nathan Jones <nathan.jones@ironmail.org>
2017-12-01 13:37:17 -0700

    Fixed import of js module

--- src/scripts/show-student-number/js/main.js
+++ src/scripts/show-student-number/js/main.js
@@ -1,13 +1,15 @@
-(function($){
-  $(document).ready(function(){
+import $ from 'jquery';
+
+export default function(studentNumber) {
+  $(() => {
     var contentHeader = $('#content').find('h1');
     if (contentHeader.length > 0) {
       // If there is more than one header, just modify the first one.
       contentHeader = contentHeader.eq(0);
       var currentText = contentHeader.text();
-      var addedText = ' (Student Number: ' + psData.studentNumber + ')';
+      var addedText = ' (Student Number: ' + studentNumber + ')';
       var newText = currentText + addedText;
       contentHeader.text(newText);
     }
-  })
-})($j);
+  });
+}
